079a221c63bda353bce72b4dff2e4103e63c26c4
CodeGuoH <2891959216@qq.com>
2020-04-09 17:19:51 +0800

    第一次提交

---
+++ src/network/模拟json数据1.js
@@ -0,0 +1,316 @@
+export default {
+  list: {
+    pop: [
+        {
+                    img: './GHimg/1/1.jpg',
+                    title: '模拟数据1',
+                    alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                    discribe: '模拟数据1'
+                  },
+        {
+                    img: './GHimg/1/2.jpg',
+                    title: '模拟数据2',
+                    alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                    discribe: '模拟数据2'
+                  },
+        {
+                    img: './GHimg/1/3.jpg',
+                    title: '模拟数据3',
+                    alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                    discribe: '模拟数据3'
+                  },
+        {
+                    img: './GHimg/1/4.jpg',
+                    title: '模拟数据4',
+                    alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                    discribe: '模拟数据4'
+                  },
+        {
+                    img: './GHimg/1/5.jpg',
+                    title: '模拟数据5',
+                    alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                    discribe: '模拟数据5'
+                  },
+        {
+                    img: './GHimg/1/6.jpg',
+                    title: '模拟数据6',
+                    alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                    discribe: '模拟数据6'
+                  },
+        {
+                    img: './GHimg/1/7.jpg',
+                    title: '模拟数据7',
+                    alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                    discribe: '模拟数据7'
+                  },
+        {
+                    img: './GHimg/1/8.jpg',
+                    title: '模拟数据8',
+                    alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                    discribe: '模拟数据8'
+                  },
+         {
+                    img: './GHimg/1/9.jpg',
+                    title: '模拟数据9',
+                    alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                    discribe: '模拟数据9'
+                  },
+        {
+                    img: './GHimg/1/10.jpg',
+                    title: '模拟数据10',
+                    alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                    discribe: '模拟数据10'
+                  },
+        {
+                    img: './GHimg/1/11.jpg',
+                    title: '模拟数据11',
+                    alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                    discribe: '模拟数据11'
+                  },
+        {
+                    img: './GHimg/1/12.jpg',
+                    title: '模拟数据12',
+                    alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                    discribe: '模拟数据12'
+                  },
+        {
+                    img: './GHimg/1/13.jpg',
+                    title: '模拟数据13',
+                    alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                    discribe: '模拟数据13'
+                  },
+        {
+                    img: './GHimg/1/14.jpg',
+                    title: '模拟数据14',
+                    alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                    discribe: '模拟数据14'
+                  },
+        {
+                    img: './GHimg/1/15.jpg',
+                    title: '模拟数据15',
+                    alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                    discribe: '模拟数据15'
+                  },
+        {
+                    img: './GHimg/1/16.jpg',
+                    title: '模拟数据16',
+                    alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                    discribe: '模拟数据16'
+                  },
+      ],
+    new: [
+       {
+                  img: './GHimg/2/1.jpg',
+                  title: '模拟数据1',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据1'
+                },
+       {
+                  img: './GHimg/2/2.jpg',
+                  title: '模拟数据2',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据2'
+                },
+       {
+                  img: './GHimg/2/3.jpg',
+                  title: '模拟数据3',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据3'
+                },
+       {
+                  img: './GHimg/2/4.jpg',
+                  title: '模拟数据4',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据4'
+                },
+       {
+                  img: './GHimg/2/5.jpg',
+                  title: '模拟数据5',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据5'
+                },
+       {
+                  img: './GHimg/2/6.jpg',
+                  title: '模拟数据6',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据6'
+                },
+       {
+                  img: './GHimg/2/7.jpg',
+                  title: '模拟数据7',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据7'
+                },
+       {
+                  img: './GHimg/2/8.jpg',
+                  title: '模拟数据8',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据8'
+                },
+       {
+                  img: './GHimg/2/9.jpg',
+                  title: '模拟数据9',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据9'
+                },
+       {
+                img: './GHimg/2/10.jpg',
+                title: '模拟数据10',
+                alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                discribe: '模拟数据10'
+              },
+       {
+                img: './GHimg/2/11.jpg',
+                title: '模拟数据11',
+                alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                discribe: '模拟数据11'
+              },
+       {
+                img: './GHimg/2/12.jpg',
+                title: '模拟数据12',
+                alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                discribe: '模拟数据12'
+              },
+       {
+                img: './GHimg/2/13.jpg',
+                title: '模拟数据13',
+                alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                discribe: '模拟数据13'
+              },
+       {
+                img: './GHimg/2/14.jpg',
+                title: '模拟数据14',
+                alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                discribe: '模拟数据14'
+              },
+       {
+                img: './GHimg/2/15.jpg',
+                title: '模拟数据15',
+                alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                discribe: '模拟数据15'
+              },
+       {
+                img: './GHimg/2/16.jpg',
+                title: '模拟数据16',
+                alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                discribe: '模拟数据16'
+              },
+       {
+                img: './GHimg/2/17.jpg',
+                title: '模拟数据17',
+                alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                discribe: '模拟数据17'
+              },
+       {
+                img: './GHimg/2/18.jpg',
+                title: '模拟数据18',
+                alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                discribe: '模拟数据18'
+              },
+    ],
+    sell: [
+         {
+                  img: './GHimg/3/1.jpg',
+                  title: '模拟数据1',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据1'
+                },
+         {
+                  img: './GHimg/3/2.jpg',
+                  title: '模拟数据2',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据2'
+                },
+         {
+                  img: './GHimg/3/3.jpg',
+                  title: '模拟数据3',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据3'
+                },
+         {
+                  img: './GHimg/3/4.jpg',
+                  title: '模拟数据4',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据4'
+                },
+         {
+                  img: './GHimg/3/5.jpg',
+                  title: '模拟数据5',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据5'
+                },
+         {
+                  img: './GHimg/3/6.jpg',
+                  title: '模拟数据6',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据6'
+                },
+         {
+                  img: './GHimg/3/7.jpg',
+                  title: '模拟数据7',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据7'
+                },
+         {
+                  img: './GHimg/3/8.jpg',
+                  title: '模拟数据8',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据8'
+                },
+         {
+                  img: './GHimg/3/9.jpg',
+                  title: '模拟数据9',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据9'
+                },
+         {
+                  img: './GHimg/3/10.jpg',
+                  title: '模拟数据10',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据10'
+                },
+         {
+                  img: './GHimg/3/11.jpg',
+                  title: '模拟数据11',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据11'
+                },
+         {
+                  img: './GHimg/3/12.jpg',
+                  title: '模拟数据12',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据12'
+                },
+         {
+                  img: './GHimg/3/13.jpg',
+                  title: '模拟数据13',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据13'
+                },
+         {
+                  img: './GHimg/3/14.jpg',
+                  title: '模拟数据14',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据14'
+                },
+         {
+                  img: './GHimg/3/15.jpg',
+                  title: '模拟数据15',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据15'
+                },
+         {
+                  img: './GHimg/3/16.jpg',
+                  title: '模拟数据16',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据16'
+                },
+         {
+                  img: './GHimg/3/17.jpg',
+                  title: '模拟数据17',
+                  alt: 'https://www.baidu.com/?tn=62095104_10_dg',
+                  discribe: '模拟数据17'
+                },
+    ]
+  }
+}
---
+++ src/views/home/Home.vue
@@ -0,0 +1,158 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <scroll class="content" ref="scroll" :probe-type="3" @scroll="contentScroll" :pull-up-load="true" @pullingUp="loadMore">
+      <home-swiper :banners="banners"/>
+      <recommend-view :recommends="recommends"/>
+      <feature-view/>
+      <tab-control class="tab-control" :titles="['流行', '新款', '精选']" @tabClick="tabClick"/>
+      <goods-list :goods="showGoods"/>
+    </scroll>
+    <back-top @click.native="backClick" v-show="isShowBackTop"/>
+  </div>
+</template>
+
+<script>
+  import NavBar from "components/common/navbar/NavBar";
+  import TabControl from "components/content/tabControl/TabControl";
+  import GoodsList from "components/content/goods/GoodsList";
+  import GoodsListItem from "components/content/goods/GoodsListItem";
+  import Scroll from "components/common/scroll/Scroll";
+  import BackTop from "components/content/backTop/BackTop";
+
+  import {getHomeMultidata, getHomeGoods, } from "network/home";
+
+  import HomeSwiper from './childCompt/HomeSwiper';
+  import RecommendView from "./childCompt/RecommendView";
+  import FeatureView from "./childCompt/FeatureView";
+
+  import data from "network/模拟json数据1"
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      TabControl,
+      GoodsList,
+      GoodsListItem,
+      Scroll,
+      BackTop
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: [],
+        goods: {
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []}
+        },
+        currentType: 'pop',
+        isShowBackTop: false
+      }
+    },
+    created() {
+      //请求多个数据
+      this.getHomeMultidata()
+      // 请求商品数据
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+
+
+    },
+    methods: {
+      /**
+       * 事件监听相关方法
+       */
+      tabClick(index) {
+        switch (index) {
+          case 0:
+            this.currentType = 'pop'
+            break
+          case 1:
+            this.currentType = 'new'
+            break
+          case 2:
+            this.currentType = 'sell'
+            break
+        }
+      },
+      backClick() {
+        this.$refs.scroll.scrollTo(0, 0)
+      },
+      contentScroll(position) {
+        this.isShowBackTop = (-position.y) > 1000
+      },
+      loadMore() {
+        //上拉加载数据
+        console.log('上拉加载更多');
+        //加载结束
+        this.$refs.scroll.scroll.finishPullUp()
+      },
+      /**
+       * 网络请求相关方法
+       */
+      //轮播图网络请求
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          this.banners = res.data.banner.list
+          this.recommends = res.data.recommend.list
+        })
+      },
+      //热点页面网络请求，因为视频教程api不能用所以以下方法可以用但是重新编辑一个方法实现模拟数据
+      // getHomeGoods(type) {
+      //   const page = this.goods[type].page + 1
+      //   getHomeGoods(type).then(res => {
+      //     this.goods[type].list.push(...res.subjects)
+      //     this.goods[type].page += 1
+      //   })
+      // }
+      getHomeGoods(type) {
+        (type == 'pop') ? this.goods[type].list.push(...data.list.pop) :
+          ((type == 'new') ? this.goods[type].list.push(...data.list.new) :
+            this.goods[type].list.push(...data.list.sell))
+        this.goods[type].list.push(...data.list.pop)
+      }
+    },
+    computed: {
+      showGoods() {
+        return this.goods[this.currentType].list
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #home {
+   padding-top: 44px;
+    height: 100vh;
+    position: relative;
+  }
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+  .tab-control {
+    position: sticky;
+    top: 44px;
+    z-index: 9;
+  }
+  .content {
+    /*height: 300px;*/
+    overflow: hidden;
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    right: 0px;
+    left: 0px;
+  }
+</style>
